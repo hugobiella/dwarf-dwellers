d13ec0d9ab639fae619ecf5beef06de1dcddfee6
hugobiella <hugo.a.biella@hotmail.com>
2026-04-15 08:13:15 -0300

    feat: add basic jump input

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,25 +3,36 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-
     public Rigidbody2D rb;
     public float moveSpeed = 5f;
 
+    [Header("Jump")]
+    public float jumpForce = 10f;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
     float horizontalMovement;
-    void Start()
-    {
+    bool isGrounded;
 
-    }
-
-    // Update is called once per frame
     void Update()
     {
         rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocity.y);
+
+        // check if ground
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
     }
 
     public void Move(InputAction.CallbackContext context)
     {
         horizontalMovement = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
     }
 }
